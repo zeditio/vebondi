a233029938210c664b07c490f6e32e2a0abe7bef
Jose Boretto blengino <joseborettoblengino@MacBook-Pro-de-Jose.local>
2017-11-14 11:21:22 -0300

    setup

---
+++ src/components/GoogleMaps.vue
@@ -0,0 +1,317 @@
+<template>
+
+<gmap-map :center="center" :zoom="13" style="height: 100vh;" :options="{styles: styles}">
+    <gmap-marker
+      :key="index"
+      v-for="(m, index) in markers"
+      :position="m.position"
+      :clickable="true"
+      :draggable="false"
+      :icon="m.icon"
+      @click=""
+    ></gmap-marker>
+</gmap-map>
+</template>
+
+<script>
+
+// ///////////////////////////////////////
+// New in 0.4.0 https://github.com/xkjyeah/vue-google-maps/blob/HEAD/API.md
+import * as VueGoogleMaps from 'vue2-google-maps'
+import Vue from 'vue'
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyD0sdRX1gWoHkmCqjsefcaowUrdBtVj2p4'
+  }
+})
+export default {
+  data () {
+    return {
+      center: {
+        lat: -31.4206274,
+        lng: -64.1908597
+      },
+      styles: [
+        {
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#ebe3cd'
+            }
+          ]
+        },
+        {
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#523735'
+            }
+          ]
+        },
+        {
+          'elementType': 'labels.text.stroke',
+          'stylers': [
+            {
+              'color': '#f5f1e6'
+            }
+          ]
+        },
+        {
+          'featureType': 'administrative',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'visibility': 'off'
+            }
+          ]
+        },
+        {
+          'featureType': 'administrative',
+          'elementType': 'geometry.stroke',
+          'stylers': [
+            {
+              'color': '#c9b2a6'
+            }
+          ]
+        },
+        {
+          'featureType': 'administrative.land_parcel',
+          'elementType': 'geometry.stroke',
+          'stylers': [
+            {
+              'color': '#dcd2be'
+            }
+          ]
+        },
+        {
+          'featureType': 'administrative.land_parcel',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#ae9e90'
+            }
+          ]
+        },
+        {
+          'featureType': 'landscape.natural',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#dfd2ae'
+            }
+          ]
+        },
+        {
+          'featureType': 'poi',
+          'stylers': [
+            {
+              'visibility': 'off'
+            }
+          ]
+        },
+        {
+          'featureType': 'poi',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#dfd2ae'
+            }
+          ]
+        },
+        {
+          'featureType': 'poi',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#93817c'
+            }
+          ]
+        },
+        {
+          'featureType': 'poi.park',
+          'elementType': 'geometry.fill',
+          'stylers': [
+            {
+              'color': '#a5b076'
+            }
+          ]
+        },
+        {
+          'featureType': 'poi.park',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#447530'
+            }
+          ]
+        },
+        {
+          'featureType': 'road',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#f5f1e6'
+            }
+          ]
+        },
+        {
+          'featureType': 'road',
+          'elementType': 'labels.icon',
+          'stylers': [
+            {
+              'visibility': 'off'
+            }
+          ]
+        },
+        {
+          'featureType': 'road.arterial',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#fdfcf8'
+            }
+          ]
+        },
+        {
+          'featureType': 'road.highway',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#f8c967'
+            }
+          ]
+        },
+        {
+          'featureType': 'road.highway',
+          'elementType': 'geometry.stroke',
+          'stylers': [
+            {
+              'color': '#e9bc62'
+            }
+          ]
+        },
+        {
+          'featureType': 'road.highway.controlled_access',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#e98d58'
+            }
+          ]
+        },
+        {
+          'featureType': 'road.highway.controlled_access',
+          'elementType': 'geometry.stroke',
+          'stylers': [
+            {
+              'color': '#db8555'
+            }
+          ]
+        },
+        {
+          'featureType': 'road.local',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#806b63'
+            }
+          ]
+        },
+        {
+          'featureType': 'transit',
+          'stylers': [
+            {
+              'visibility': 'off'
+            }
+          ]
+        },
+        {
+          'featureType': 'transit.line',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#dfd2ae'
+            }
+          ]
+        },
+        {
+          'featureType': 'transit.line',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#8f7d77'
+            }
+          ]
+        },
+        {
+          'featureType': 'transit.line',
+          'elementType': 'labels.text.stroke',
+          'stylers': [
+            {
+              'color': '#ebe3cd'
+            }
+          ]
+        },
+        {
+          'featureType': 'transit.station',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#dfd2ae'
+            }
+          ]
+        },
+        {
+          'featureType': 'water',
+          'elementType': 'geometry.fill',
+          'stylers': [
+            {
+              'color': '#b9d3c2'
+            }
+          ]
+        },
+        {
+          'featureType': 'water',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'color': '#92998d'
+            }
+          ]
+        }
+      ],
+      markers: [
+        {
+          position: {
+            lat: -31.421128,
+            lng: -64.188362
+          }
+        },
+        {
+          position: {
+            lat: -31.425128,
+            lng: -64.185362
+          },
+          icon: {
+            path: 'M18,11H6V6H18M16.5,17A1.5,1.5 0 0,1 15,15.5A1.5,1.5 0 0,1 16.5,14A1.5,1.5 0 0,1 18,15.5A1.5,1.5 0 0,1 16.5,17M7.5,17A1.5,1.5 0 0,1 6,15.5A1.5,1.5 0 0,1 7.5,14A1.5,1.5 0 0,1 9,15.5A1.5,1.5 0 0,1 7.5,17M4,16C4,16.88 4.39,17.67 5,18.22V20A1,1 0 0,0 6,21H7A1,1 0 0,0 8,20V19H16V20A1,1 0 0,0 17,21H18A1,1 0 0,0 19,20V18.22C19.61,17.67 20,16.88 20,16V6C20,2.5 16.42,2 12,2C7.58,2 4,2.5 4,6V16Z',
+            fillColor: '#00a665',
+            scale: 1,
+            fillOpacity: 1
+          }
+        }
+      ]
+    }
+  },
+  methods: {
+    showBusStopMarker: function () {
+      console.log('hola')
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+                // Call resizePreserveCenter() on all maps
+      Vue.$gmapDefaultResizeBus.$emit('resize')
+    }
+  }
+}
+</script>
