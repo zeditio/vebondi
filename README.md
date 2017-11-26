09d4456ec39f2c79cde2be6eab6ad1f491c7465b
Jose Boretto blengino <joseborettoblengino@MacBook-Pro-de-Jose.local>
2017-11-26 11:10:01 -0300

    1 - Discovery

--- README.md
+++ README.md
@@ -1,27 +1,57 @@
 # vebondi
-
 > Version mobile de http://cordoba.cuandollega.net/
 
-## Build Setup
+# Design + Code Process
+https://www.youtube.com/watch?v=sJhhLvW-Xvg
 
+###  1 - Discovery
+### People
+
++ Who is the owner?
+  + I and municipality of cordoba
++ Why does the owner want this?
+  + I: to learn the whole process of creating an application, learning technologies and frontend design, something that the people use everyday.
+  + muni: offer a better transport service
++ Who is the audience?
+  + people who use the public transport service every day.
++ Why does the owner want this?
+  + They do not want to wait for the bus, sometimes it's too cold or too hot, sometimes the wait is long, sometimes it's not safe to be outside.
+
+### Content Strategy
+
++ What feature the audience need?
+ + Select a bus stop
+ + Get the time to the next bus
+ + Favorites stops
+ + Near Bus stops
+ + Less battery consume
+ + See all bus of one stop (x2)
+ + Unlimited request
+ + Confidence in next bus time
+ + More Intuitive
+ + See a map
+ + Have all bus lines
+ + Loading time
+ + Order Favorites stops
+
+
++ What was their feedback?
+
+https://play.google.com/store/apps/details?id=efisat.cuandollega.cordoba
+
+## Build Setup
 ``` bash
 # install dependencies
 npm install
-
 # serve with hot reload at localhost:8080
 npm run dev
-
 # build for production with minification
 npm run build
-
 # build for production and view the bundle analyzer report
 npm run build --report
-
 # run unit tests
 npm run unit
-
 # run all tests
 npm test
 ```
-
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/components/GoogleMaps.vue
+++ src/components/GoogleMaps.vue
@@ -1,22 +1,23 @@
 <template>
 
-<gmap-map :center="center" :zoom="13" style="height: 100vh;" :options="{styles: styles}">
+<gmap-map :center="center" :zoom="13" style="height: 100vh;" :options="{styles: styles, streetViewControl: false}">
     <gmap-marker
-      :key="index"
-      v-for="(m, index) in markers"
-      :position="m.position"
-      :clickable="true"
-      :draggable="false"
-      :icon="m.icon"
-      @click=""
-    ></gmap-marker>
-</gmap-map>
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="false"
+        :icon="m.icon"
+        @click=""
+      ></gmap-marker>
+    </gmap-map>
 </template>
 
 <script>
 
 // ///////////////////////////////////////
-// New in 0.4.0 https://github.com/xkjyeah/vue-google-maps/blob/HEAD/API.md
+// New in 0.4.0
+// https://github.com/xkjyeah/vue-google-maps/blob/HEAD/API.md
 import * as VueGoogleMaps from 'vue2-google-maps'
 import Vue from 'vue'
 Vue.use(VueGoogleMaps, {
@@ -33,30 +34,6 @@ export default {
       },
       styles: [
         {
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'color': '#ebe3cd'
-            }
-          ]
-        },
-        {
-          'elementType': 'labels.text.fill',
-          'stylers': [
-            {
-              'color': '#523735'
-            }
-          ]
-        },
-        {
-          'elementType': 'labels.text.stroke',
-          'stylers': [
-            {
-              'color': '#f5f1e6'
-            }
-          ]
-        },
-        {
           'featureType': 'administrative',
           'elementType': 'geometry',
           'stylers': [
@@ -66,91 +43,10 @@ export default {
           ]
         },
         {
-          'featureType': 'administrative',
-          'elementType': 'geometry.stroke',
-          'stylers': [
-            {
-              'color': '#c9b2a6'
-            }
-          ]
-        },
-        {
-          'featureType': 'administrative.land_parcel',
-          'elementType': 'geometry.stroke',
-          'stylers': [
-            {
-              'color': '#dcd2be'
-            }
-          ]
-        },
-        {
-          'featureType': 'administrative.land_parcel',
-          'elementType': 'labels.text.fill',
-          'stylers': [
-            {
-              'color': '#ae9e90'
-            }
-          ]
-        },
-        {
-          'featureType': 'landscape.natural',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'color': '#dfd2ae'
-            }
-          ]
-        },
-        {
           'featureType': 'poi',
           'stylers': [
             {
               'visibility': 'off'
-            }
-          ]
-        },
-        {
-          'featureType': 'poi',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'color': '#dfd2ae'
-            }
-          ]
-        },
-        {
-          'featureType': 'poi',
-          'elementType': 'labels.text.fill',
-          'stylers': [
-            {
-              'color': '#93817c'
-            }
-          ]
-        },
-        {
-          'featureType': 'poi.park',
-          'elementType': 'geometry.fill',
-          'stylers': [
-            {
-              'color': '#a5b076'
-            }
-          ]
-        },
-        {
-          'featureType': 'poi.park',
-          'elementType': 'labels.text.fill',
-          'stylers': [
-            {
-              'color': '#447530'
-            }
-          ]
-        },
-        {
-          'featureType': 'road',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'color': '#f5f1e6'
             }
           ]
         },
@@ -164,118 +60,10 @@ export default {
           ]
         },
         {
-          'featureType': 'road.arterial',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'color': '#fdfcf8'
-            }
-          ]
-        },
-        {
-          'featureType': 'road.highway',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'color': '#f8c967'
-            }
-          ]
-        },
-        {
-          'featureType': 'road.highway',
-          'elementType': 'geometry.stroke',
-          'stylers': [
-            {
-              'color': '#e9bc62'
-            }
-          ]
-        },
-        {
-          'featureType': 'road.highway.controlled_access',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'color': '#e98d58'
-            }
-          ]
-        },
-        {
-          'featureType': 'road.highway.controlled_access',
-          'elementType': 'geometry.stroke',
-          'stylers': [
-            {
-              'color': '#db8555'
-            }
-          ]
-        },
-        {
-          'featureType': 'road.local',
-          'elementType': 'labels.text.fill',
-          'stylers': [
-            {
-              'color': '#806b63'
-            }
-          ]
-        },
-        {
           'featureType': 'transit',
           'stylers': [
             {
               'visibility': 'off'
-            }
-          ]
-        },
-        {
-          'featureType': 'transit.line',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'color': '#dfd2ae'
-            }
-          ]
-        },
-        {
-          'featureType': 'transit.line',
-          'elementType': 'labels.text.fill',
-          'stylers': [
-            {
-              'color': '#8f7d77'
-            }
-          ]
-        },
-        {
-          'featureType': 'transit.line',
-          'elementType': 'labels.text.stroke',
-          'stylers': [
-            {
-              'color': '#ebe3cd'
-            }
-          ]
-        },
-        {
-          'featureType': 'transit.station',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'color': '#dfd2ae'
-            }
-          ]
-        },
-        {
-          'featureType': 'water',
-          'elementType': 'geometry.fill',
-          'stylers': [
-            {
-              'color': '#b9d3c2'
-            }
-          ]
-        },
-        {
-          'featureType': 'water',
-          'elementType': 'labels.text.fill',
-          'stylers': [
-            {
-              'color': '#92998d'
             }
           ]
         }
@@ -285,6 +73,12 @@ export default {
           position: {
             lat: -31.421128,
             lng: -64.188362
+          },
+          icon: {
+            path: 'M18,11H6V6H18M16.5,17A1.5,1.5 0 0,1 15,15.5A1.5,1.5 0 0,1 16.5,14A1.5,1.5 0 0,1 18,15.5A1.5,1.5 0 0,1 16.5,17M7.5,17A1.5,1.5 0 0,1 6,15.5A1.5,1.5 0 0,1 7.5,14A1.5,1.5 0 0,1 9,15.5A1.5,1.5 0 0,1 7.5,17M4,16C4,16.88 4.39,17.67 5,18.22V20A1,1 0 0,0 6,21H7A1,1 0 0,0 8,20V19H16V20A1,1 0 0,0 17,21H18A1,1 0 0,0 19,20V18.22C19.61,17.67 20,16.88 20,16V6C20,2.5 16.42,2 12,2C7.58,2 4,2.5 4,6V16Z',
+            fillColor: '#00a665',
+            scale: 1,
+            fillOpacity: 1
           }
         },
         {
@@ -303,13 +97,13 @@ export default {
     }
   },
   methods: {
-    showBusStopMarker: function () {
+    getMarkers: function () {
       console.log('hola')
     }
   },
   watch: {
     '$route' (to, from) {
-                // Call resizePreserveCenter() on all maps
+      // Call resizePreserveCenter() on all maps
       Vue.$gmapDefaultResizeBus.$emit('resize')
     }
   }
