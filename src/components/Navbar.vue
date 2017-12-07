7a16c3f2c043e04cb335dcbe93141c0782150f5d
Jose Boretto blengino <joseborettoblengino@MacBook-Pro-de-Jose.local>
2017-12-06 22:11:21 -0300

    vue router hsitory and hash mode

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,29 @@
-<!-- https://medium.com/front-end-hacking/vue-js-mobile-navbar-using-vuetify-803856f00dfd -->
+
+
 <template type="text/babel">
 
-<v-card height="100vh">
-    <router-view></router-view>
-    <v-bottom-nav height="10vh" absolute :value="true" :active.sync="e1" color="transparent">
-      <v-btn flat color="teal" value="fav" href="/fav" router>
-        <span>Favoritos</span>
-        <v-icon>favorite</v-icon>
-      </v-btn>
-      <v-btn flat color="cyan" value="arribos" href="/arribos" router>
-        <span>Proximos</span>
-        <v-icon>timer</v-icon>
-      </v-btn>
-      <v-btn flat color="green" value="saldo" href="/saldo" router>
-        <span>Saldo</span>
-        <v-icon>attach_money</v-icon>
-      </v-btn>
-    </v-bottom-nav>
-</v-card>
+<div>
+    <v-card height="100vh">
+        <div>
+            <router-view></router-view>
+            <v-bottom-nav height="10vh" absolute :value="true" :active.sync="e1" color="transparent">
+                <v-btn flat color="teal" value="fav" href="/fav" router>
+                    <span>Favoritos</span>
+                    <v-icon>favorite</v-icon>
+                </v-btn>
+                <v-btn flat color="cyan" value="arribos" href="/arribos" router>
+                    <span>Proximos</span>
+                    <v-icon>timer</v-icon>
+                </v-btn>
+                <v-btn flat color="green" value="saldo" href="/saldo" router>
+                    <span>Saldo</span>
+                    <v-icon>attach_money</v-icon>
+                </v-btn>
+            </v-bottom-nav>
+    </v-card>
+    <h2>navbar</h2>
+    </div>
+</div>
 
 </template>
 
