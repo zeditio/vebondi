c60da1286b159fa0fb367c38430bfbfffe5c3cc6
Jose Boretto blengino <joseborettoblengino@MacBook-Pro-de-Jose.local>
2017-12-09 15:09:43 -0300

    - service worker fix - now js deploy fix - icons added

--- build/service-worker-prod.js
+++ build/service-worker-prod.js
@@ -5,19 +5,27 @@
   // and that the current page is accessed from a secure origin. Using a
   // service worker from an insecure origin will trigger JS console errors.
   var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
-      // [::1] is the IPv6 localhost address.
-      window.location.hostname === '[::1]' ||
-      // 127.0.0.1/8 is considered localhost for IPv4.
-      window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-      )
-    );
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    ) ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^192\.168\./
+    )
+  );
 
   window.addEventListener('load', function() {
-      if ('serviceWorker' in navigator &&
-          (window.location.protocol === 'https:' || isLocalhost)) {
-        navigator.serviceWorker.register('service-worker.js')
+
+    console.log("isLocalhost", isLocalhost);
+
+    if ('serviceWorker' in navigator &&
+      (window.location.protocol === 'https:' || isLocalhost)) {
+      navigator.serviceWorker.register('service-worker.js')
         .then(function(registration) {
+          console.log("Service Worker Registered");
           // updatefound is fired if service-worker.js changes.
           registration.onupdatefound = function() {
             // updatefound is also fired the very first time the SW is installed,
@@ -39,7 +47,7 @@
 
                   case 'redundant':
                     throw new Error('The installing ' +
-                                    'service worker became redundant.');
+                      'service worker became redundant.');
 
                   default:
                     // Ignore
@@ -50,6 +58,6 @@
         }).catch(function(e) {
           console.error('Error during service worker registration:', e);
         });
-      }
+    }
   });
 })();
