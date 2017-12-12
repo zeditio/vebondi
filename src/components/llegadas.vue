1b06ee086466652038815d4cd3a3c17bd3bba3e7
Jose Boretto Blengino <joseboretto@gmail.com>
2017-12-12 18:35:40 -0300

    mensaje agregar paradas favoritas

--- src/components/llegadas.vue
+++ src/components/llegadas.vue
@@ -1,23 +1,26 @@
 
 
 <template type="text/babel">
-  <v-layout column>
-      <v-flex xs12>
+
+<v-layout column>
+    <v-flex xs12>
         <v-container fluid grid-list-md>
-          <v-layout row wrap>
-            <template v-for="(arrival, i) in myArrivals">
-              <bus-arrival-card
-              :card-name="arrival.cardName"
-              :stop-code="arrival.stopCode"
-              :bus-lines="arrival.busLines"
-              :buttons="buttons"
-              isVisible
-              @deleteCard="deleteCard"/>
-            </template>
-          </v-layout>
+            <v-layout row wrap>
+                <template v-if="myArrivals.length == 0">
+                    <v-flex xs12 class="pt-5">
+                        <h2 class="text-xs-center grey--text"> No hay paradas favoritas</h2>
+                    </v-flex>
+                    <v-flex xs12>
+                        <h4 class="text-xs-center grey--text"> Busca y agrega haciendo click en la lupa</h4>
+                    </v-flex>
+                </template>
+                <template v-for="(arrival, i) in myArrivals">
+                    <bus-arrival-card :card-name="arrival.cardName" :stop-code="arrival.stopCode" :bus-lines="arrival.busLines" :buttons="buttons" isVisible @deleteCard="deleteCard" />
+                </template>
+            </v-layout>
         </v-container>
-      </v-flex>
-  </v-layout>
+    </v-flex>
+</v-layout>
 
 </template>
 
@@ -28,16 +31,14 @@ export default {
   name: 'llegadas',
   data () {
     return {
-      myArrivals: [
-        {
-          cardName: 'casa al trabajo',
-          stopCode: 'C5643',
-          busLines: [{
-            line: 71,
-            llegadas: ['Arribando', '10 min', '15 min']
-          }]
-        }
-      ],
+      myArrivals: [{
+        cardName: 'casa al trabajo',
+        stopCode: 'C5643',
+        busLines: [{
+          line: 71,
+          llegadas: ['Arribando', '10 min', '15 min']
+        }]
+      }],
       buttons: ['delete', 'refresh']
     }
   },
